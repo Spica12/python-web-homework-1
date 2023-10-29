b7a86171a979e128cb5347ba333e2b9649ec492d
Vitalii Savenko <vitaly.savenko12@gmail.com>
2023-10-29 21:49:53 +0200

    work

--- FinalProject/__main__.py
+++ FinalProject/__main__.py
@@ -1,24 +1,48 @@
+from abc import abstractmethod, ABC
 from Bot import Bot
 
 
+class Output(ABC):
+
+    @abstractmethod
+    def output(self, result):
+        pass
+
+
+class TerminalOutput(Output):
+    
+    def __init__(self):
+        self.delimiter = '-----'
+
+    def output(self, result: str):
+        print(self.delimiter, result, self.delimiter, sep='\n')
+
+
 if __name__ == "__main__":
-    print('Hello. I am your contact-assistant. What should I do with your contacts?')
+    terminal = TerminalOutput()
+    terminal.output('Hello. I am your contact-assistant. What should I do with your contacts?')
     bot = Bot()
     bot.book.load("auto_save")
     commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
     while True:
         action = input('Type help for list of commands or enter your command\n').strip().lower()
         if action == 'help':
+            result = ''
             format_str = str('{:%s%d}' % ('^',20))
             for command in commands:
-                print(format_str.format(command))
-            action = input().strip().lower()
-            bot.handle(action)
-            if action in ['add', 'remove', 'edit']:
-                bot.book.save("auto_save")
+                result = '\n'.join([result, format_str.format(command)])
+            terminal.output(result)
+            # action = input().strip().lower()
+            # result = bot.handle(action)
+            # if action in ['add', 'remove', 'edit']:
+            #     bot.book.save("auto_save")
+            # terminal.output(result)
         else:
-            bot.handle(action)
+            result = bot.handle(action)
             if action in ['add', 'remove', 'edit']:
                 bot.book.save("auto_save")
+            terminal.output(result)
         if action == 'exit':
+            terminal.output('Goodbye!')
             break
+        
